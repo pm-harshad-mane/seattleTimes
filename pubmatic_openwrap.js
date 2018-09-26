9d55457a90c786828c58823083635eef853483d4
Harshad Mane <harshad.mane@pubmatic.com>
2018-09-26 14:33:15 -0700

    Update pubmatic_openwrap.js

--- pubmatic_openwrap.js
+++ pubmatic_openwrap.js
@@ -19,7 +19,7 @@ PWT.jsLoaded = function(){ //PubMatic pwt.js on load callback is used to load GP
   })();
 };
 
-///*
+/*
 (function() {
   var purl = window.location.href;
   var url = '//ads.pubmatic.com/AdServer/js/pwt/156708/981';
